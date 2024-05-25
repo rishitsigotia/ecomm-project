3e49201262da50097f106422ee9fca13e12cea7b
Rishit <rsigotia123@gmail.com>
2024-05-25 15:33:08 +0530

    Routing module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SellerAuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
