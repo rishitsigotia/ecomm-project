e59b8117b8aecf1ebcdf1bcb238b6e637ce6d95b
Rishit <rsigotia123@gmail.com>
2024-06-20 01:58:33 +0530

    Logout function and seller name display

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,7 +10,7 @@ export class HeaderComponent implements OnInit {
 
   constructor(private route: Router) { }
   menuType:string = "default";
-
+  sellerName:string ="";
   ngOnInit(): void {
     this.route.events.subscribe((val: any) => {
       if(val.url)
@@ -18,6 +18,9 @@ export class HeaderComponent implements OnInit {
         if(localStorage.getItem('seller') && val.url.includes('seller'))
         {
           this.menuType = "seller"
+          let sellerStore = localStorage.getItem('seller');
+          let sellerDate = sellerStore && JSON.parse(sellerStore)[0]; //checking if exist then parse it
+          this.sellerName = sellerDate.name;
         }else
         {
           this.menuType="default"
@@ -25,5 +28,8 @@ export class HeaderComponent implements OnInit {
       }
     })
   }
-
+  logout(){
+    localStorage.removeItem('seller');// remove the seller stored in local
+    this.route.navigate(['/']); // after removing redirect it to home page  
+  }
 }
