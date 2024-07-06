bea5f4c43f4e6620530ab3cc34b44089e86e3216
Rishit <rsigotia123@gmail.com>
2024-07-07 00:45:16 +0530

    Delete Product API integration

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -11,12 +11,33 @@ export class SellerHomeComponent implements OnInit {
 
   constructor(private product : ProductService) { }
   productList : undefined | product[];
+  productMessage : undefined| string;
 
   ngOnInit(): void {
-  this.product.productList().subscribe((result)=>{
-    this.productList = result;
-  })    
+  this.showList();
   }
   
+  deleteProduct(id:number){
+    if(id != null){
+      this.product.deleteProduct(id).subscribe((result) =>
+      {
+        if(result)
+        {
+          this.productMessage = "Product is Deleted";
+          this.showList();
+          }
+      }
+      )
+    }
+    setTimeout(() =>{
+      this.productMessage = undefined;
+    },3000)
+  }
+
+  showList(){
+    this.product.productList().subscribe((result)=>{
+      this.productList = result;
+    })  
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,23 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { product } from '../data-type';
 import { retry } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient , private route : Router) { }
 
   addProduct(data:product)
   {
    return this.http.post("http://localhost:3000/products",data,{observe:'response'}).
     subscribe((result) => {
-
+      this.route.navigate(["seller-home"]);
   });
   }
   productList()
   {
     return this.http.get<product[]>('http://localhost:3000/products')
+  }
+
+  deleteProduct(id:number){
+    
+    return this.http.delete(`http://localhost:3000/products/${id}`)
   }
 }
