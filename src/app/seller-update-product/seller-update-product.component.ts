f094dc1dc99d37a0e6896db4d024c604314248ff
Rishit <rsigotia123@gmail.com>
2024-07-07 01:30:26 +0530

    Populating product list by api call and binding using ngModel

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { product } from '../data-type';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -7,10 +9,14 @@ import { product } from '../data-type';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent implements OnInit {
-
-  constructor() { }
+  productData : undefined | product
+  constructor(private route : ActivatedRoute , private product : ProductService) { }
 
   ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id')
+    productId && this.product.getProduct(productId).subscribe((data) =>{
+      this.productData = data;
+    })
   }
   update(data:product){
     
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -27,4 +27,8 @@ export class ProductService {
     
     return this.http.delete(`http://localhost:3000/products/${id}`)
   }
+
+  getProduct(id:string){
+    return this.http.get<product>(`http://localhost:3000/products/${id}`)
+  }
 }
